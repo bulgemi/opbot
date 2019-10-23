1219a50c4dd3c8a875c4d37f5e372f1e10db7515
donghun <bulgemi77@gmail.com>
2019-10-23 23:17:45 +0900

    테스트 내용 수정.

--- opbot/chatbot/test/tdd_pdfkit.py
+++ opbot/chatbot/test/tdd_pdfkit.py
@@ -5,6 +5,8 @@ import pdfkit
 pdfkit.from_url('http://google.com', 'google.pdf')
 pdfkit.from_string('hello<br/>hello', 'hello.pdf')
 pdfkit.from_file('top.txt', 'top.pdf')
+pdfkit.from_file('b099220d-f56a-11e9-b5df-d0abd5335702.txt',
+                 'b099220d-f56a-11e9-b5df-d0abd5335702.pdf')
 
 with open("top.txt", "r") as f:
     data = ""
