d56092ab50bbac2be7f4e00909381454d5512623
donghun <bulgemi77@gmail.com>
2019-10-25 17:53:58 +0900

    인사 처리.

--- opbot/chatbot/app/rtm_controller.py
+++ opbot/chatbot/app/rtm_controller.py
@@ -43,6 +43,11 @@ class AsyncRtm(Resource):
         recommend_tasks = None
 
         if len(command_list) <= 0:
+            hello_messages = [
+                "네, 안녕하세요.",
+                "Hi.",
+                "안녕하세요."
+            ]
             err_messages = [
                 "죄송해요. 다시 입력해주세요.",
                 "죄송해요. 알수 없는 명령입니다.",
@@ -50,46 +55,56 @@ class AsyncRtm(Resource):
                 "Sorry, Please try again.",
                 "죄송해요. 이해를 못 했습니다."
             ]
-            current_app.bot.put_broadcast(channel=rtm_msg['channel'],
-                                          message=err_messages[randrange(len(err_messages))])
+            hello = current_app.bot.say_hello(rtm_msg['message'])
+
+            if len(hello) <= 0:
+                current_app.bot.put_broadcast(channel=rtm_msg['channel'],
+                                              message=err_messages[randrange(len(err_messages))])
+            else:
+                current_app.bot.put_broadcast(channel=rtm_msg['channel'],
+                                              message=hello_messages[randrange(len(hello_messages))])
         else:
             in_channel_id = current_app.bot.get_in_channel_info(rtm_msg['channel'], 'C')
-            recommend_tasks = current_app.bot.task_recommend(in_channel_id)
-
-            # current_app.logger.debug("recommend_tasks=<%r>" % recommend_tasks)
 
         for task in command_list:
             current_app.logger.debug("task=<%r>" % task)
 
             if task == '!조치!':
                 # context 상태 변경.
-                current_app.bot.set_context_s()
+                current_app.bot.set_context_s(rtm_msg['channel'])
                 # 조치 Task
                 # 3.분석 task 추천 정보 조회
-                anal_tasks = current_app.bot.task_recommend(in_channel_id)
+                anal_tasks = current_app.bot.task_recommend(in_channel_id, rtm_msg['channel'])
                 current_app.bot.put_chat(channel=rtm_msg['channel'], message=None, tasks=anal_tasks)
             elif task == '!분석!':
                 # context 상태 변경.
-                current_app.bot.set_context_a()
-                anal_tasks = current_app.bot.task_recommend(in_channel_id)
+                current_app.bot.set_context_a(rtm_msg['channel'])
+                anal_tasks = current_app.bot.task_recommend(in_channel_id, rtm_msg['channel'])
                 current_app.bot.put_chat(channel=rtm_msg['channel'], message=None, tasks=anal_tasks)
             elif task == '!종료!':
                 # context 상태 변경.
-                current_app.bot.set_context_a()
+                current_app.bot.set_context_a(rtm_msg['channel'])
                 msg = "상황 종료되었습니다. 감사합니다."
                 # 전체 채널에 공지
                 out_channels = current_app.bot.channel_read(in_channel_id)
+
                 if len(out_channels) > 0:
                     for channel_info in out_channels:
                         if channel_info[0] == 'B':
-                            current_app.bot.put_broadcast(channel=channel_info[1], message=msg)
+                            current_app.bot.put_end(channel=channel_info[1], message=msg)
                         elif channel_info[0] == 'C':
-                            current_app.bot.put_broadcast(channel=channel_info[1], message=msg)
+                            current_app.bot.put_end(channel=channel_info[1], message=msg)
                 break
             else:
+                recommend_tasks = current_app.bot.task_recommend(in_channel_id, rtm_msg['channel'])
+                # current_app.logger.debug("recommend_tasks=<%r>" % recommend_tasks)
                 task_id = current_app.bot.get_task_id(recommend_tasks, task)
 
-                result = manage.task_execute.delay(task_id, rtm_msg['channel'])
-                result.wait()
+                if task_id is None:
+                    current_app.bot.put_broadcast(channel=rtm_msg['channel'],
+                                                  message="죄송해요. 알수 없는 명령입니다.")
+                else:
+                    result = manage.task_execute.delay(task_id, rtm_msg['channel'])
+                    result.wait()
 
         return res_msg, 201
