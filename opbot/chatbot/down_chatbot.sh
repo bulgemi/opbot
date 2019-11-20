bcc390229e338b4cc7e3e03c8bd606be915a7c6d
donghun <bulgemi77@gmail.com>
2019-11-20 17:47:59 +0900

    chatbot down script 수정.

--- opbot/chatbot/down_chatbot.sh
+++ opbot/chatbot/down_chatbot.sh
@@ -2,4 +2,4 @@
 cd $OPBOT_HOME/chatbot;
 ps -ef |grep python |grep asyncio_receiver |grep -v grep |awk '{print $2}' |xargs kill -9;
 ps -ef |grep chatbot_celery |grep -v grep |awk '{print $2}' |xargs kill -9;
-ps -ef |grep manage:app |grep -v grep |awk '{print $2}' |xargs kill -9;
+ps -ef |grep manage:app | grep 9595 |grep -v grep |awk '{print $2}' |xargs kill -9;
