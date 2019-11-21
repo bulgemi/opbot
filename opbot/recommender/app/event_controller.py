ff749931f8725da0fa9ee214bfafa1db987b7ce0
donghun <bulgemi77@gmail.com>
2019-11-21 11:18:51 +0900

    응답 구조 추가.

--- opbot/recommender/app/event_controller.py
+++ opbot/recommender/app/event_controller.py
@@ -3,19 +3,20 @@ __author__ = 'kim dong-hun'
 import sys
 import os
 from flask import current_app
-from flask_restplus import Resource
+from flask_restplus import Resource, fields, marshal_with
 from .apis.v1_.event import EventDto
 sys.path.append(os.getenv('OPBOT_HOME'))
 
 event_a = EventDto.api
 event_m = EventDto.event
+mfields = {'task_id': fields.Raw, 'percentage': fields.Raw}
 
 
 @event_a.route('/')
 class AsyncEvent(Resource):
     @event_a.doc('create a New Recommend Event')
     @event_a.expect(event_m)
-    @event_a.marshal_with(event_m, code=201)
+    @event_a.marshal_with(mfields, code=201)
     def post(self):
         """
         create a new Recommend event
@@ -30,11 +31,8 @@ class AsyncEvent(Resource):
                                                        event_a.payload))
 
         event_info = event_a.payload
+        recommender = current_app.recommender
+        res = recommender.recommend_info_get(event_info['event_msg'], event_info['action_type'])
+        current_app.logger.debug("res=<%r>" % res)
 
-        # 2.사용가능한 TASK 조회
-
-        # 3.TASK 추천도(백분율) 계산
-
-        # 4.추천도 기준으로 정렬된 TASK LIST 반환
-
-        return event_a.payload, 201
+        return res, 201
