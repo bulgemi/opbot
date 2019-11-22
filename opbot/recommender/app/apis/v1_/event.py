b0d094bcf47ef721e6dc993bb72718733c6eba33
donghun <bulgemi77@gmail.com>
2019-11-22 10:36:36 +0900

    REST API Web 제공.

--- opbot/recommender/app/apis/v1_/event.py
+++ opbot/recommender/app/apis/v1_/event.py
@@ -7,3 +7,5 @@ class EventDto(object):
     api = Namespace('events', description='Recommender REST APIs')
     event = api.model('event', {"action_type": fields.String(required=True, description="'A' or 'S'"),
                                 "event_msg": fields.String(required=True, description='Event Message')})
+    resource_fields = api.model('recommend_tasks', {'task_id': fields.Raw,
+                                                    'percentage': fields.Raw})
