385a1dda010e987a8eeeba86787400d5f21ea7f0
donghun <bulgemi77@gmail.com>
2020-03-27 16:12:20 +0900

    RSA 환경설정 추가.

--- opbot/manager/app/config.py
+++ opbot/manager/app/config.py
@@ -8,3 +8,6 @@ Manager 환경 설정 파일.
 class Config(object):
     LOG_LEVEL = 'debug'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://opbot_appl:apbot_appl26#!@localhost/opbot_db?charset=utf8'
+    # RSA KEY
+    PUBLIC_KEY = ''
+    PRIVATE_KEY = ''
