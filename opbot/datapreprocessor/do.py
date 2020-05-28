3a6a1a9d442ed8b39c664df705646b32d08a4acb
donghun <bulgemi77@gmail.com>
2020-05-29 02:17:46 +0900

    메시지 수정.

--- opbot/datapreprocessor/do.py
+++ opbot/datapreprocessor/do.py
@@ -1,8 +1,13 @@
 # _*_ coding: utf-8 _*_
 __author__ = 'kim dong-hun'
 from .step1 import Step1
+from .step2 import Step2
 
 if __name__ == "__main__":
-    dp = Step1("datapreprocessor/data/Rims_history.xlsx")
-    dp.clear_data()
-    dp.read_xls()
+    s1 = Step1("datapreprocessor/data/Rims_history.xlsx")
+    s1.clear_data()
+    s1.read_xls()
+    s2 = Step2("datapreprocessor/data/Rims_history.xlsx")
+    s2.create_csv()
+    s2.read_xls()
+    s2.close_csv()
