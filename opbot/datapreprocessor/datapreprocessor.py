e2165c3be316d2c54d3edf218dc383b62015c281
donghun <bulgemi77@gmail.com>
2020-05-29 01:16:16 +0900

    전처리 모듈.

--- opbot/datapreprocessor/datapreprocessor.py
+++ opbot/datapreprocessor/datapreprocessor.py
@@ -1,10 +1,17 @@
 # _*_ coding: utf-8 _*_
 __author__ = 'kim dong-hun'
+import pandas as pd
 
 
 class DataPreprocessor(object):
-    def __init__(self):
-        pass
+    def __init__(self, xlsx):
+        self.data = pd.read_excel(xlsx)
+        self.type = None
+        self.key_value = ""
+        self.measure = False
+        self.s_close = False
+        self.contents = []
+        self.key_map = {"통합품질감시": 0, "장애의심": 1, "상황": 2, "장애": 3}
 
     def __del__(self):
         pass
@@ -12,17 +19,19 @@ class DataPreprocessor(object):
     def read_xls(self):
         pass
 
-    def get_type(self):
-        pass
+    def get_type(self, row):
+        return row['장애구분']
 
-    def get_send_message(self):
-        pass
+    def get_send_message(self, row):
+        return row['발송메시지']
 
     def parse_row(self):
         pass
 
-    def find_key_word(self):
+    def find_key_word(self, key_word):
         pass
 
-    def data_filter(self):
-        pass
+    def data_filter(self, row):
+        import re
+        f_row = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》{}]', '', row)
+        return f_row
