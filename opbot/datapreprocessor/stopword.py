0eec046fa118832609f3d899d066cb890bd401cd
donghun <bulgemi77@gmail.com>
2020-07-28 18:20:20 +0900

    데이터 전처리 추가.

---
+++ opbot/datapreprocessor/stopword.py
@@ -0,0 +1,79 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'kim dong-hun'
+import pandas as pd
+import re
+import os
+import csv
+
+
+class StopWord(object):
+    def __init__(self, csv_file):
+        """
+        불용어 초기화.
+        :param csv_file:
+        """
+        self.data = pd.read_csv(csv_file)
+        self.stat_map = {"통합품질감시": 0, "장애의심": 0, "상황": 0, "장애": 0}
+        self.stop_words = [
+            r'\D{2,4}?(선임|수석|매니저|메니저)',
+            r'\d{2,3}[:\.\s]*\d{2,2}(?!\d)',
+            r'\(\d{3}\)\s*\d{4}[-\.\s]??\d{4}',
+            r'\d{1,3}[,\.]\d{1,3}',
+            r'\d{1,4}[년]\d{1,2}[월]\d{1,2}[일]',
+            r'\d{1,4}[년]\s\d{1,2}[월]\s\d{1,2}[일]',
+            r'\d{1,2}[월]\s\d{1,2}[일]',
+            r'\d{1,2}[월]\d{1,2}[일]',
+            r'IT종합상황실 상황관리자'
+        ]
+        self.fp = open(os.getenv('OPBOT_HOME') + "/datapreprocessor/data/Rims_stopword.csv", 'w')
+
+    def __del__(self):
+        self.fp.close()
+
+    def remove_special_characters(self, row):
+        """
+        특수문자 제거.
+        :param row:
+        :return:
+        """
+        f_row = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》{}_●]', '', row)
+        return f_row
+
+    def do_stopword(self, row):
+        """
+        불용어 처리
+        :param row:
+        :return:
+        """
+        for r in self.stop_words:
+            row = re.sub(r, '', row)
+        return row
+
+    def data_info(self):
+        print(self.data.shape)
+        print("%r" % self.stat_map)
+
+    def read_column(self):
+        csv_fp = csv.writer(self.fp, delimiter=',')
+
+        for i, r in self.data.iterrows():
+            self.stat_map[r[0]] += 1
+            # print("%r=%r" % (i, r[2]))
+            row = self.do_stopword(r[2])
+            rsc = self.remove_special_characters(row)
+            # print("%r" % rsc)
+            # print("%r=%r" % (i, r[3]))
+            if pd.isnull(r[3]):
+                csv_fp.writerow([r[0], rsc, r[3]])
+            else:
+                result_row = self.do_stopword(r[3])
+                result_rsc = self.remove_special_characters(result_row)
+                csv_fp.writerow([r[0], rsc, result_rsc])
+                # print("%r" % r[2])
+                # print("%r" % rsc)
+
+
+if __name__ == '__main__':
+    stopword = StopWord("data/Rims_result.csv")
+    stopword.read_column()
+    stopword.data_info()
