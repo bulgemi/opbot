8ea27522e7628ef820a98466c75f6eb5ca529717
donghun <bulgemi77@gmail.com>
2020-07-28 18:19:31 +0900

    데이터 전처리 추가.

---
+++ opbot/datapreprocessor/tf-idf.py
@@ -0,0 +1,48 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'kim dong-hun'
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class TfIdf(object):
+    def __init__(self, pickle_file):
+        """
+        tf-idf 초기화.
+        :param pickle_file:
+        """
+        with open(pickle_file, 'rb') as f:
+            self.data = pickle.load(f)
+        self.tfidf_matrix = None
+
+    def do(self):
+        """
+
+        :return:
+        """
+        tfidf_vectorizer = TfidfVectorizer(min_df=10)
+        self.tfidf_matrix = tfidf_vectorizer.fit_transform(self.data)
+        text = tfidf_vectorizer.get_feature_names()
+        idf = tfidf_vectorizer.idf_
+        print("text(%r): %r" % (len(text), text))
+        print("idf(%r): %r" % (len(idf), idf))
+
+    def similarity(self, index):
+        """
+        cosine_similarity
+        :param index:
+        :return:
+        """
+        for i, r in enumerate(self.data):
+            similarity = cosine_similarity(self.tfidf_matrix[index], self.tfidf_matrix[i])[0][0]
+
+            if similarity > 0.80:
+                print("similarity: ", similarity)
+                print(self.data[index])
+                print(self.data[i])
+
+
+if __name__ == '__main__':
+    tf_idf = TfIdf("data/tokenized_text.pickle")
+    tf_idf.do()
+    tf_idf.similarity(2578)
