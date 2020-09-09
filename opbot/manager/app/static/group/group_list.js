f51bf8a85a03e01ba2d50c089b49caf25948c80f
donghun <bulgemi77@gmail.com>
2020-09-09 10:03:56 +0900

    삭제, 검색 처리 추가.

--- opbot/manager/app/static/group/group_list.js
+++ opbot/manager/app/static/group/group_list.js
@@ -3,4 +3,31 @@
     author: kim dong-hun
 */
 $(document).ready(function() {
+    // Search 버튼.
+    $('#search').bind('click', function() {
+        var cond1 = $("#cond1 option:selected").val();
+        var cond2 = $("#cond2 option:selected").val();
+        var some =  $("#some").val();
+        console.log("click search.");
+
+        $.ajax({
+            url: $SCRIPT_ROOT + '/group/_search_group_list',
+            type: 'POST',
+            contentType: "application/json",
+            dataType: 'JSON',
+            data: JSON.stringify({
+                cond1: cond1,
+                cond2: cond2,
+                some: some
+            }),
+            success: function(response) {
+                console.log(response.result);
+                // location.reload(true);
+                $("#groupList").jsGrid("option", "data", response.result);
+            },
+            error: function(xtr, status, error) {
+                console.log(xtr +":"+status+":"+error);
+            }
+        });
+    });
 });
