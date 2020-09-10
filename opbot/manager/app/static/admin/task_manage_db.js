8e0b97d8f66cec8b91a3a3c7c7236d7226cb1ce5
donghun <bulgemi77@gmail.com>
2020-09-10 20:41:58 +0900

    task 관리 기능 추가.

---
+++ opbot/manager/app/static/admin/task_manage_db.js
@@ -0,0 +1,49 @@
+/*
+    file: task_manage_db.js
+    author: kim dong-hun
+*/
+(function() {
+    var db = {
+        loadData: function(filter) {
+            console.log("filter=", filter);
+
+            taskUid = $('#task_uid').val();
+            var d = $.Deferred();
+
+            $.ajax({
+                url: $SCRIPT_ROOT + '/admin/_load_task_list',
+                type: 'POST',
+                contentType: "application/json",
+                dataType: "JSON",
+                data: JSON.stringify({
+                    task_uid: taskUid,
+                    filter: filter
+                }),
+            }).done(function(response) {
+                d.resolve(response.result);
+            });
+
+            return d.promise();
+        },
+        insertItem: function(insertingTask) {
+            console.log("insertingTask=", insertingTask);
+        },
+        updateItem: function(updatingTask) {
+            console.log("updatingTask=", updatingTask)
+        },
+        deleteItem: function(deletingTask) {
+            console.log("deletingTask=", deletingTask)
+            $.ajax({
+                url: $SCRIPT_ROOT + '/admin/_delete_task',
+                type: 'POST',
+                contentType: "application/json",
+                dataType: "JSON",
+                data: JSON.stringify(deletingTask),
+            }).done(function(response) {
+                console.log("response=", response);
+            });
+        }
+    };
+
+    window.db = db;
+}());
