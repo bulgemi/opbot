c4a31fe581bfa43e16fb86724ac36abb1757784c
donghun <bulgemi77@gmail.com>
2020-09-23 01:42:28 +0900

    시스템 사용량 분석 기능 추가.

--- opbot/chatbot/manage.py
+++ opbot/chatbot/manage.py
@@ -35,6 +35,28 @@ def task_execute(task_id, out_channel_id, task_name, task_type, target_list, pla
 
 
 @celery.task()
+def cpu_execute(out_channel_id, node_list):
+    """
+    CPU Usage 생성 비동기 수행.(Async Task Queue)
+    :param out_channel_id:
+    :param node_list:
+    :return:
+    """
+    return current_app.bot.cpu_execute(out_channel_id, node_list)
+
+
+@celery.task()
+def mem_execute(out_channel_id, node_list):
+    """
+    Memory Usage 생성 비동기 수행.(Async Task Queue)
+    :param out_channel_id:
+    :param node_list:
+    :return:
+    """
+    return current_app.bot.mem_execute(out_channel_id, node_list)
+
+
+@celery.task()
 def put_collector(event_uid, task_id, exec_type):
     """
     Collector 에 사용자 이벤트 전송.(Async Task Queue)
